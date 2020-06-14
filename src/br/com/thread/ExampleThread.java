0dcf1de86694808f6f97209c54085f0a4aa55d60
Wanderlea Lodi <wgnoato@gmail.com>
2020-06-14 17:43:49 -0300

    Runnable Example

--- src/br/com/thread/ExampleThread.java
+++ src/br/com/thread/ExampleThread.java
@@ -7,49 +7,53 @@ public class ExampleThread {
 	public static void main(String[] args) throws InterruptedException {
 
 		// Thread processing in parallel 1
-		new Thread() {
-
-			public void run() {
-
-				// parallel code
-				for (int pos = 0; pos < 10; pos++) {
-
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					System.out.println("teste----teste");
-
-				}
-			};
-
-		}.start();
+		Thread thread1 = new Thread(threadexample1);
+		thread1.start();
 		
 		// Thread processing in parallel 2
-
-		new Thread() {
-
-			public void run() {
-
-				// parallel code
-				for (int pos = 0; pos < 10; pos++) {
-
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					System.out.println("teste2----teste2");
-
-				}
-			};
-
-		}.start();
+		Thread thread2 = new Thread(threadexample2);
+		thread2.start();
+		
 
 		System.out.println("Thread end");
 		JOptionPane.showMessageDialog(null, "system keeps running for the user");
 
 	}
+	
+	private static Runnable threadexample1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			// parallel code
+			for (int pos = 0; pos < 10; pos++) {
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("teste----teste");
+			}
+		}
+	};
+	
+	private static Runnable threadexample2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			// parallel code
+			for (int pos = 0; pos < 10; pos++) {
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("teste2----teste2");
+
+			}
+		}
+	};
+		
 
 }
